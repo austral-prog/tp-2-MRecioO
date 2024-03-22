182e3763873a6dbfffb6803cd2b9cc90a58d1562
MRecioO <162721939+MRecioO@users.noreply.github.com>
2024-03-22 11:46:47 -0300

    Recio ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,15 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+
+    name = first_name + " " + last_name
+ 
+    print(name.lower())
+
+    print(name.title())
+
+    print(name.upper())
+
+    print("\t" + name.lower())
+
+ada()
